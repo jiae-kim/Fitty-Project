667c9cc28b1044ce8b4700b42ed1cd2c0514f161
cruella13 <100200569+cruella13@users.noreply.github.com>
2022-09-07 16:43:39 +0900

    [손민진]  add : 메일 vo클래스

---
+++ src/main/java/com/project/fitty/mail/model/vo/Remail.java
@@ -0,0 +1,9 @@
+package com.project.fitty.mail.model.vo;
+
+public class Remail {
+	private int emNo;
+	private String emRecipient;
+	private String emRead;
+	private String status;
+	private String emReference;
+}
