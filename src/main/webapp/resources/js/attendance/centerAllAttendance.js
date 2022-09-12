d41ecb93f2616f79630e9db5c9fb9548f336a72f
kimalbab <kimalbab@gmail.com>
2022-09-12 09:22:42 +0900

    [이소희] 근태전체조회 js 수정

--- src/main/webapp/resources/js/attendance/centerAllAttendance.js
+++ src/main/webapp/resources/js/attendance/centerAllAttendance.js
@@ -1,14 +1,14 @@
-/**
- * centerAllAttendance 페이지
- */
- $(function(){
-      		
-	selectAllAttList(1);
-    
+$(function(){
+
+    selectAllAttList(1);
+
     let month = Number($("#thisMonth").val());
     let year = Number($("#thisYear").val());
-	
-	$(document).ready(function(){
+    let today = new Date();
+	let nowYear = today.getFullYear();
+	let nowMonth = today.getMonth();
+
+    $(document).ready(function(){
     	$("#nextBtn").on("click",function() {
     		
     		if(month < 12) {
@@ -41,17 +41,13 @@
     		}
     		})
     })
-        
+
 })
 
-
-
-
-
 function selectAllAttList(page){
-	
-	$.ajax({        			
-		url: "attList.att",
+
+    $.ajax({
+        url: "attList.att",
 		data:{
 		    // searchType:$("#searchType").val(),
 		    // searchText:$("#searchText").val(),
@@ -60,67 +56,59 @@ function selectAllAttList(page){
 		    thisYear : $("#thisYear").val()
 		  	
 		},
-		type:"post",
-		success:function(result){
-			
-			let value = "";
-			let countValue = "";
-			let pageValue = "";
-			
-			
-			let empList = result.empList;
-			let pi = result.pi;
-			
+        type:"post",
+        success:function(result){
+            let value = "";
+            let countValue = "";
+            let pageValue = "";
 
-			
-			if(empList.length == 0){
-				value += "<tr>"
+            let empList = result.empList;
+            let countList = result.countList;
+            let pi = result.pi;
+
+            if(empList.length == 0) {
+                value += "<tr>"
 				       + "<td colspan='32'> 직원이 없습니다. </td>"
                          + "</tr>";
-			}else{
-				
-				for(let i=0; i<empList.length; i++){
+            } else {
+                for(let i=0; i<empList.length; i++){
 					
-				value += "<tr>"
-					   +    "<th><input type='checkbox' name='choiceAll' id='choiceAll' onclick='checkAll();'></th>"
-                       +  	"<td>" +  empList[i].empName + "</td>"
-                     for(let k=0; k<32; k++){  
-                	 // for(var k in empList[i].attList) {
-                	 		if(empList[i].attList[k].attStatus == "" || typeof empList[i].attList[k].attStatus == "undefined" || empList[i].attList[k].attStatus == null ){
-    							value += 	"<td>　</td>"          	 		
-                	 		} else {
-							    value += 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].attList[k].attStatus + "</button></td>"
-                	 		}
-						}
-			            
-              
-                       
-                value += "</tr>";
+                    value += "<tr>"
+                        +    "<th><input type='checkbox' name='choiceAll' id='choiceAll' onclick='checkAll();'></th>"
+                        +  	"<td>" +  empList[i].empName + "</td>"
+                        
+                        for(var k in empList[i].attList) {
+                                value += 	"<td>" + empList[i].attList[k].attStatus + "</td>"
+                            }
+                            
+                
+                        
+                    value += "</tr>";
                      
 				}
-				
-				
-			if(empList.length == 0){
-				countValue += "<tr>"
-				       			+ "<td colspan='5'> 직원이 없습니다. </td>"
-                       	   + "</tr>";
-			}else{
-				
-				for(let i=0; i<empList.length; i++){
-					
-				countValue += "<tr>"
-                       
-                	  		 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countX + "</button></td>"
-						     + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countL + "</button></td>"
-						     + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countE + "</button></td>"
-						     + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countYH + "</button></td>"
-						     + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countV + "</button></td>"
-						     + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countPtime + "</button></td>"
-						}
-                       
-                countValue += "</tr>";
-                     
-				}
+
+                if(empList.length == 0){
+                    countValue += "<tr>"
+                                       + "<td colspan='5'> 직원이 없습니다. </td>"
+                                  + "</tr>";
+                }else{
+                    
+                    for(let i=0; i<empList.length; i++){
+                        
+                    countValue += "<tr>"
+                           
+                                   + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countX + "</button></td>"
+                                 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countL + "</button></td>"
+                                 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countE + "</button></td>"
+                                 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countYH + "</button></td>"
+                                 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countV + "</button></td>"
+                                 + 	"<td><button type='button' value='" + empList[i].empNo + "' id='listedEmpNoBtn'>" + empList[i].countList[i].countPtime + "</button></td>"
+                            }
+                           
+                    countValue += "</tr>";
+                         
+                    }
+
 				
 				if(pi.currentPage == 1){
         			// 현재페이지가 1페이지면 < 버튼 disabled
@@ -144,20 +132,16 @@ function selectAllAttList(page){
 		         } else {
 		        	  pageValue +=	"<li class='page-item next'><button class='page-link' onclick='selectAllAttList(" + (pi.currentPage + 1) + ")'><i class='tf-icon bx bx-chevron-right'></i></button></li>"
 		        	  
-		         
-			}
-			
-			$("#memListTBody").html(value);
-			$(".pagination").html(pageValue);
-			$("#countListTBody").html(countValue);
-			}
-		},
-		error:function(){
-			console.log("관리자 예약내역리스트 조회용 ajax통신 실패");
-		}
-		
-	});
-	
+		         }
+                 
+                 $("#memListTBody").html(value);
+                 $("#countListTBody").html(countValue);
+                 $(".pagination").html(pageValue);
+            }
+        },
+        error:function(){
+            console.log("관리자 예약내역리스트 조회용 ajax통신 실패"); 
+        }
+    })
+
 }
-
-
