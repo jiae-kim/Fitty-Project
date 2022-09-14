713ba5bce98e0e1a6127b3f09e622326edd37622
dahye1231 <dahye1231@naver.com>
2022-09-14 12:14:18 +0900

    [정다혜] 수업등록

--- src/main/java/com/project/fitty/ptclass/model/service/ClassService.java
+++ src/main/java/com/project/fitty/ptclass/model/service/ClassService.java
@@ -2,16 +2,21 @@ package com.project.fitty.ptclass.model.service;
 
 import java.util.ArrayList;
 
+import com.project.fitty.ptclass.model.vo.PtClass;
 import com.project.fitty.ptclass.model.vo.Reply;
 import com.project.fitty.user.model.vo.User;
 
 public interface ClassService {
 	
 	
-	//수업등록
-//	User selectUser(int userNo);
+	//수업등록 페이지 회원조회
+	User selectUser(int userNo);
 	
-	int insertClass(int userNo);
+	//수업등록
+	int insertClass(PtClass pt);
+	
+	//수업등록 완료 상태로 변경
+	int updateStatus(int userNo);
 
 	
 	//댓글 리스트 서비스
