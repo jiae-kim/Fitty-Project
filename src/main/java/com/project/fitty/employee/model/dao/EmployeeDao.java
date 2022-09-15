e03fc5149475d7b8152b5a24f4135b23c0dc5679
cruella13 <100200569+cruella13@users.noreply.github.com>
2022-09-15 22:10:11 +0900

    [손민진] update empMyPage

--- src/main/java/com/project/fitty/employee/model/dao/EmployeeDao.java
+++ src/main/java/com/project/fitty/employee/model/dao/EmployeeDao.java
@@ -89,11 +89,6 @@ public class EmployeeDao {
 		return sqlSession.update("employeeMapper.deleteEmployee", empNo);
 	}
 	
-	// 직원 정보조회
-	public int selectEmployee(SqlSessionTemplate sqlSession, Employee e) {
-		
-		return sqlSession.selectOne("employeeMapper.selectEmployee", e);
-	}
 	
 	// 직원 정보수정
 	public int updateEmployee(SqlSessionTemplate sqlSession, Employee e) {
--- src/main/java/com/project/fitty/employee/model/service/EmployeeService.java
+++ src/main/java/com/project/fitty/employee/model/service/EmployeeService.java
@@ -48,11 +48,7 @@ public interface EmployeeService {
 	
 	// 선택된 리스트에서 원하는 리스트 조회
 	ArrayList<Employee> deleteEmpNoList(String empNo);
-	
 
-	// 직원 정보 조회(select)
-	int selectEmployee(Employee e);
- 	
 	// 직원 정보 수정 (update)
 	int updateEmployee(Employee e);
 
